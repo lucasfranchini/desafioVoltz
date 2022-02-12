ee6b6e41014736ad073c5cea7f8931fdb08a9914
lucas franchini <lucasfranchini.a@gmail.com>
2022-02-12 18:52:43 -0300

    feat: setup mongoose

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import "@/setup";
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
+import mongoose from "mongoose";
 import errorHandlingMiddleware from "./middlewares/errorHandlingMiddleware";
 
 const app = express();
@@ -9,5 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use(errorHandlingMiddleware);
+
+export async function init() {
+  await mongoose.connect(process.env.DATABASE_URL, {
+    authSource: "admin",
+  });
+}
 
 export default app;
