359adc234176c28b4a2bb54be19d9ef2853ae65a
lucas franchini <lucasfranchini.a@gmail.com>
2022-02-12 14:28:11 -0300

    feat: initial server setup

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import "@/setup";
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+export default app;
