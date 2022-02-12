1e45dbbc768a609aa543138892acfcfe6380e8fe
lucas franchini <lucasfranchini.a@gmail.com>
2022-02-12 19:51:28 -0300

    feat: user routers

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import "@/setup";
+
+import errorHandlingMiddleware from "@/middlewares/errorHandlingMiddleware";
+import router from "@/routers";
+
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
 import mongoose from "mongoose";
-import errorHandlingMiddleware from "./middlewares/errorHandlingMiddleware";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(router);
 
 app.use(errorHandlingMiddleware);
 
