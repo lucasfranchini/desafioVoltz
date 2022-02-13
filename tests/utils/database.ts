7cd3bac0de2a005773c66ce468a7447232f1cd3b
lucas franchini <lucasfranchini.a@gmail.com>
2022-02-13 01:49:19 -0300

    feat: database utils

--- tests/utils/database.ts
+++ tests/utils/database.ts
@@ -1,5 +1,11 @@
 import "../../src/setup";
+import mongoose from "mongoose";
+import { Tools } from "../../src/models/tools";
 
-export async function clearDatabase() {}
+export async function clearDatabase() {
+  await Tools.deleteMany();
+}
 
-export async function endConnection() {}
+export async function endConnection() {
+  await mongoose.disconnect();
+}
