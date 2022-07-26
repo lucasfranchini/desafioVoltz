431aeea7ca2f77f064a03fcf1fbcbc08fb6e0a19
Lucas Franchini <lucasfranchini.a@gmail.com>
2022-07-26 14:31:13 -0300

    feat prod deploy

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -4,5 +4,8 @@ import toolsRouter from "@/routers/tools";
 const router = Router();
 
 router.use("/tools", toolsRouter);
+router.get("/", (req, res) => {
+  res.send({ name: "teste" });
+});
 
 export default router;
